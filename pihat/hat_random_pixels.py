9d63ca94a1995ed62f3b7c4b0eb612961653933a
chardonnae <chardonnae@example.com>
2019-07-23 11:03:05 -0500

    adding my python scripts

---
+++ pihat/hat_random_pixels.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# this script will display colors for individual pixels on the Pi HAT
+from sense_hat import SenseHat
+import random
+
+red = (5, 0, 0)
+yellow = (0, 5, 0)
+blue = (0, 0, 5)
+
+pixels
+ 
